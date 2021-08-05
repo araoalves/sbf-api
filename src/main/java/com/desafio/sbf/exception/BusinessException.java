b6355fc9366202bcaa4e668596de1243fa82ad0e
afarias <arao.alves7@gmail.com>
2021-08-05 17:33:00 -0300

    Criando classe de exceção para as regras de negócio

---
+++ src/main/java/com/desafio/sbf/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.desafio.sbf.exception;
+
+public class BusinessException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException(String s) {
+        super(s);
+    }
+
+    public BusinessException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+}
