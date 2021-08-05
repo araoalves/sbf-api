a85de8aa267f5b6a55b0a1373b0e8ec5b0c2202b
afarias <arao.alves7@gmail.com>
2021-08-05 19:17:16 -0300

    Criando teste para consumir API de cotação

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 **Tecnologias Utilizadas**
 
+- &#9745; Spring Boot (v2.5.3)
 - &#9745; JAVA 11
 - &#9745; Postgres SQL
 - &#9745; Documentação com Swagger
 - &#9745; RestTemplate consumo de APIs Externas
+- &#9745; [API](https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,INR-BRL) externa de cotação
 
--- src/test/java/com/desafio/sbf/services/ConsumerTest.java
+++ src/test/java/com/desafio/sbf/services/ConsumerTest.java
@@ -9,16 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
+import com.desafio.sbf.model.cotacao.CotacaoExterna;
 import com.desafio.sbf.service.consumer.Consumer;
+import com.desafio.sbf.service.util.Utilitarios;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ConsumerTest {
 
-    @Autowired
-    private Consumer consumer;
+	private final Utilitarios utilitarios;
+    private final Consumer consumer;
 
     @Test
     public void deveConsumirApiExterna() throws Exception {
@@ -28,7 +32,13 @@ public class ConsumerTest {
 
         TestJson o = (TestJson) consumer.get("http://echo.jsontest.com/key/value/one/two", TestJson.class, MediaType.APPLICATION_JSON, headers);
         Assertions.assertNotNull(o);
-    }	
+    }
+    
+    @Test
+    public void deveConsumirCotacaoApiExterna() throws Exception {
+    	CotacaoExterna cotacao = (CotacaoExterna) consumer.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,INR-BRL", CotacaoExterna.class, MediaType.APPLICATION_JSON, utilitarios.criarCabecalhoRequisicao());
+        Assertions.assertNotNull(cotacao);
+    }
 }
 
 @Data
